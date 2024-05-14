f333a39d67f9112bd0df021fdeacaa188f1d92cf
jieun <heyfer6867@gmail.com>
2024-05-14 20:38:07 +0900

    application.yml 수정, MemberRepositoryTest 수정-에러해결

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
@@ -18,6 +19,7 @@ public class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
+    @Transactional //추가해줌
     public void testMember() throws Exception{
         //given
         Member member = new Member();
