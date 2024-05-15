403c639c138ab417ae8ee341eba8461715d3f63c
jieun <heyfer6867@gmail.com>
2024-05-15 19:45:32 +0900

    Delivery,OrderItem,OrderStatus 생성, Address,Member,Order 코드 추가

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,18 +1,24 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "memebr_id")
     private Long id;
     private String name;
+
+    @Embedded //@Embeddable이거나 둘중 하나만 있어도 됨.둘다 쓰는걸 권장. 내장타입인지 확인용
     private Address address;
+    @OneToMany(mappedBy = "member") //읽기 전용이 된다는 거
     private List<Order> orders = new ArrayList<>();
 }
