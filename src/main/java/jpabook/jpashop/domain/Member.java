ad7909a25a8d165e46e2653154d839c3c517253c
jieun <heyfer6867@gmail.com>
2024-05-15 16:37:01 +0900

    Member 코드 작성

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,4 +1,14 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private Address address;
 }
