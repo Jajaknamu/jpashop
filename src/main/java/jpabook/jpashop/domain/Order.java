c91d8a61802bcbbbd5ae99dfe2af7a390d02eb38
jieun <heyfer6867@gmail.com>
2024-05-17 15:53:49 +0900

    Order.java 조회로직,OrderItem.java 비즈니스 로직, 생성메서드 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -65,4 +65,30 @@ public class Order {
         order.setOrderDate(LocalDateTime.now());
         return order;
     }
+
+    //비즈니스 로직
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소 불가");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem:orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //조회 로직
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
