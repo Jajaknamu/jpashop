bbe989fa2f35824ee82ed48eb464f0283797e09a
jieun <heyfer6867@gmail.com>
2024-05-23 16:37:33 +0900

    변경감지 하는거

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -28,4 +28,19 @@ public class ItemService {
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
+
+    /**
+     * 영속성 컨텍스트가 자동 변경
+     * @param id
+     * @param name
+     * @param price
+     * @param stockQuantity
+     */
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
 }
